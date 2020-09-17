66ed54a632a756c6aa428b433c54d517f4c474ac
Paul Ponomarev <rocksquare@gmail.com>
2020-09-17 17:02:58 +0300

    transaction updt

--- src/main/java/project/dao/AbstractDao.java
+++ src/main/java/project/dao/AbstractDao.java
@@ -4,14 +4,16 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
+@Transactional
 public abstract class AbstractDao {
 
     @Autowired
     SessionFactory sessionFactory;
 
     public Session getSession() {
-        return sessionFactory.openSession();
+        return sessionFactory.getCurrentSession();
     }
 }
