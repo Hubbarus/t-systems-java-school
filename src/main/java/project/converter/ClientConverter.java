9ebc7a547cf402b37839cea6e5203d7e2111cfcd
Paul Ponomarev <rocksquare@gmail.com>
2020-09-25 18:21:16 +0300

    @transactional annotation removed from classes

--- src/main/java/project/converter/ClientConverter.java
+++ src/main/java/project/converter/ClientConverter.java
@@ -3,12 +3,10 @@ package project.converter;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import project.dto.ClientDTO;
 import project.entity.Client;
 
 @Service
-@Transactional
 public class ClientConverter {
 
     @Autowired
--- src/main/java/project/converter/OrderConverter.java
+++ src/main/java/project/converter/OrderConverter.java
@@ -3,7 +3,6 @@ package project.converter;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import project.dto.CartDTO;
 import project.dto.OrderDTO;
 import project.entity.Cart;
@@ -14,7 +13,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
-@Transactional
 public class OrderConverter {
 
     @Autowired
